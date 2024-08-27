11ad1143c498fd89f226b88bba8310cd52c17fdd
LukasPodhorny <112276946+LukasPodhorny@users.noreply.github.com>
2024-08-27 13:13:10 +0200

    update

--- Assets/Scripts/TerrainGeneration/MeshGenerator.cs
+++ Assets/Scripts/TerrainGeneration/MeshGenerator.cs
@@ -23,6 +23,7 @@ public class MeshGenerator : MonoBehaviour
 
     [Range(1, 20)]
     public float view_distance;
+    public float unit_size;
     public Transform player;
 
     Vector3 player_pos;
@@ -34,7 +35,7 @@ public class MeshGenerator : MonoBehaviour
     public string DATA_NAME = "terrain_noise_data";
     public float height_multiplier;
     float[] noisemap;
-    Color[] gradient_1024;
+    Color[] gradient_2048;
     NoiseParameters terrain_noise_parameters;
     public ComputeShader noiseCompute;
 
@@ -42,16 +43,16 @@ public class MeshGenerator : MonoBehaviour
     {
         terrain_noise_parameters = JsonHelper.LoadClass<NoiseParameters>(DATA_NAME);
 
-        gradient_1024 = NoiseMapGenerator.PreEvGradient(terrain_noise_parameters.color_map, 1024);
+        gradient_2048 = NoiseMapGenerator.PreEvGradient(terrain_noise_parameters.color_map, 2048);
 
         float temp = Time.realtimeSinceStartup;
         max_lod = lods[0];
         noisemap = NoiseMapGenerator.GenerateCombinedNoiseMapGpu(terrain_noise_parameters, 1000, 1000, max_lod, noiseCompute);
-        print(Mathf.RoundToInt((Time.realtimeSinceStartup - temp)*1000) + "ms it took to compute world noisemap on gpu");
+        print(Mathf.RoundToInt((Time.realtimeSinceStartup - temp) * 1000) + "ms it took to compute world noisemap on gpu");
 
         float temp2 = Time.realtimeSinceStartup;
         GenerateChunks();
-        print(Mathf.RoundToInt((Time.realtimeSinceStartup - temp2)*1000) + "ms it took to calculate mesh cpu");
+        print(Mathf.RoundToInt((Time.realtimeSinceStartup - temp2) * 1000) + "ms it took to calculate mesh cpu");
     }
     void Update()
     {
@@ -60,14 +61,14 @@ public class MeshGenerator : MonoBehaviour
 
     void OnDrawGizmosSelected()
     {
-        Gizmos.color = new Color(255, 0, 0, 0.2f);
+        Gizmos.color = new Color(0, 255, 0, 0.2f);
         Vector3Int[] points = PointsInViewDist(view_distance, chunk_size);
         for (int i = 0; i < points.Length; i++)
         {
-            Gizmos.DrawCube(points[i] + new Vector3Int(chunk_size / 2, 0, chunk_size / 2), new Vector3(chunk_size - 0.5f, 0.2f, chunk_size - 0.5f));
+            Gizmos.DrawCube((Vector3)points[i] * unit_size + new Vector3(chunk_size / 2, 0, chunk_size / 2) * unit_size, new Vector3(chunk_size * unit_size - 2, 0.2f, chunk_size * unit_size - 2));
         }
     }
-    
+
     void GenerateChunks()
     {
         points = PointsInViewDist(view_distance, chunk_size);
@@ -77,12 +78,12 @@ public class MeshGenerator : MonoBehaviour
         {
             int lod_group = Mathf.Max(Mathf.Abs(points[i].x), Mathf.Abs(points[i].z)) / chunk_size;
 
-            chunks[i] = new Chunk(chunk_size, chunk_size, lods[lod_group], height_multiplier, mesh_material, lod_group);
-            chunks[i].chunk_object.transform.position = points[i];
+            chunks[i] = new Chunk(chunk_size, chunk_size, lods[lod_group], mesh_material, lod_group, unit_size);
+            chunks[i].chunk_object.transform.position = (Vector3)points[i] * unit_size;
 
 
             Vector3Int chunk_offset = (points[i] + new Vector3Int(400, 0, 400)) * max_lod;
-            chunks[i].mesh_chunk.UpdateMeshNoiseMap1D(noisemap, chunk_offset, height_multiplier, max_lod, 1000, gradient_1024);
+            chunks[i].mesh_chunk.UpdateMeshNoiseMap1D(noisemap, chunk_offset, height_multiplier * unit_size, max_lod, 1000, gradient_2048);
             chunks[i].UpdateCollider();
         }
     }
@@ -90,17 +91,17 @@ public class MeshGenerator : MonoBehaviour
     void UpdateChunks()
     {
         player_pos = player.transform.position;
-        player_grid_pos = ToGridPos(player_pos, chunk_size);
+        player_grid_pos = ToGridPos(player_pos, chunk_size * unit_size);
 
         if (player_grid_pos != player_last_grid_pos)
         {
-            
+
             for (int i = 0; i < chunks.Length; i++)
             {
-                chunks[i].chunk_object.transform.position = points[i] + player_grid_pos * chunk_size;
+                chunks[i].chunk_object.transform.position = (Vector3)points[i] * unit_size + (Vector3)player_grid_pos * chunk_size * unit_size;
                 Vector3Int chunk_offset = (points[i] + player_grid_pos * chunk_size + new Vector3Int(400, 0, 400)) * max_lod;
-        
-                chunks[i].mesh_chunk.UpdateMeshNoiseMap1D(noisemap, chunk_offset, height_multiplier, max_lod, 1000, gradient_1024);
+
+                chunks[i].mesh_chunk.UpdateMeshNoiseMap1D(noisemap, chunk_offset, height_multiplier * unit_size, max_lod, 1000, gradient_2048);
                 chunks[i].UpdateCollider();
             }
         }
@@ -130,13 +131,13 @@ public class MeshGenerator : MonoBehaviour
         }
         return points.ToArray();
     }
-    
+
     bool InsideCircle(int x, int y, float radius)
     {
         float distance = Mathf.Sqrt(x * x + y * y);
         return distance <= radius;
     }
-    Vector3Int ToGridPos(Vector3 pos, int unit_size)
+    Vector3Int ToGridPos(Vector3 pos, float unit_size)
     {
         return new Vector3Int((int)Mathf.Floor(pos.x / unit_size), 0, (int)Mathf.Floor(pos.z / unit_size));
     }
@@ -147,18 +148,20 @@ class Chunk
     public Mesh mesh;
     public GameObject chunk_object;
     public float lod_group;
-    public Chunk(int xsize, int ysize, float lod, float amplitude, Material mat, int lod_group)
+    public float unit_size;
+    public Chunk(int xsize, int ysize, float lod, Material mat, int lod_group, float unit_size)
     {
         this.lod_group = lod_group;
-        
-        mesh_chunk = new MeshChunk(xsize, ysize, lod, amplitude);
+        this.unit_size = unit_size;
+
+        mesh_chunk = new MeshChunk(xsize, ysize, lod, unit_size);
         mesh = mesh_chunk.GenerateMesh();
 
         chunk_object = new GameObject();
         chunk_object.AddComponent<MeshFilter>().mesh = mesh;
         chunk_object.AddComponent<MeshRenderer>().sharedMaterial = mat;
-        
-        if(lod_group == 0||lod_group == 1)
+
+        if (lod_group == 0 || lod_group == 1)
         {
             chunk_object.AddComponent<MeshCollider>();
         }
@@ -166,7 +169,7 @@ class Chunk
 
     public void UpdateCollider()
     {
-        if(lod_group == 0||lod_group == 1)
+        if (lod_group == 0 || lod_group == 1)
         {
             chunk_object.GetComponent<MeshCollider>().sharedMesh = mesh;
         }
